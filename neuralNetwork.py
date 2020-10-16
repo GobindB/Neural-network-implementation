1013c001fc23f01925c6b8051334d7e6b8db5f4e
Gobind <gobind@thesinghs.org>
2020-10-16 12:39:54 -0700

    network class def

---
+++ neuralNetwork.py
@@ -0,0 +1,20 @@
+
+import numpy
+
+class neuralNetwork:
+
+	def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+		# set number of nodes
+		self.inodes = inputNodes
+		self.hnodes = hiddenNodes
+		self.onodes = outputNodes
+		#set learning rate
+		self.lr = learningRate
+
+	# train the network
+	def train():
+		pass
+
+	# query the network
+	def query():
+		pass
