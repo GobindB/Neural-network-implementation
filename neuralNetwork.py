b8432f46f4492d821eeddc1e58002d3fc73ea8f7
Gobind <gobind@thesinghs.org>
2020-10-16 17:03:37 -0700

    add node weight randmoization

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -11,6 +11,12 @@ class neuralNetwork:
 		#set learning rate
 		self.lr = learningRate
 
+		# initialize random weights of thw form w_i_j from node i to j
+		# initial random wieghts centered around 0 with stdev related to
+		# number of incoming node links 1/sqrt(n)
+		self.wih = (numpy.random.normal(0.0, pow(self.inodes), self.hnodes, self.inodes) - 0.5)
+		self.who = (numpy.random.normal(0.0, pow(self.hnodes), self.onodes, self.hnodes) - 0.5)
+
 	# train the network
 	def train():
 		pass
