8fbd329bfac80d548368a39f4aa714e646a0dd49
Gobind <gobind@thesinghs.org>
2020-11-02 16:19:08 -0800

    query func

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,28 +1,49 @@
 
 import numpy
+from numpy.core.fromnumeric import ndim
 import scipy.special
+
 
 class neuralNetwork:
 
-	def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
-		# set number of nodes
-		# TODO: add matrix size validation 
-		self.inodes = inputNodes
-		self.hnodes = hiddenNodes
-		self.onodes = outputNodes
-		# set learning rate
-		self.lr = learningRate
-		# initialize random weights of thw form w_i_j from node i to j
-		# initial random wieghts centered around 0 with stdev related to
-		# number of incoming node links 1/sqrt(n)
-		self.wih = (numpy.random.normal(0.0, pow(self.inodes), self.hnodes, self.inodes) - 0.5)
-		self.who = (numpy.random.normal(0.0, pow(self.hnodes), self.onodes, self.hnodes) - 0.5)
+    def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+        # set number of nodes
+        # TODO: add matrix size validation
+        self.inodes = inputNodes
+        self.hnodes = hiddenNodes
+        self.onodes = outputNodes
+        # set learning rate
+        self.lr = learningRate
+        # initialize random weights of thw form w_i_j from node i to j
+        # initial random wieghts centered around 0 with stdev related to
+        # number of incoming node links 1/sqrt(n)
+        self.wih = numpy.random.normal(
+            0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(
+            0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
 
-	# train the network
-	def train():
-		pass
+        # sigmoid functions
+        self.activation_func = lambda x: scipy.special.expit(x)
 
-	# query the network
-	def query(inputs):
-		# matrix of combined moderated signals into each hidden layer node
-		hidden_inputs = numpy.dot(self.wih, inputs)
+
+    # train the network
+    def train():
+        pass
+
+    # query the network
+    def query(self, inputs_list):
+        # matrix of combined moderated signals into each hidden layer node
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+		
+		# calculate signals emerging from hidden layers
+        hidden_outputs = self.activation_func(hidden_inputs)
+
+		# calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+
+		# calculate signals coming from the outer layer
+
+        return self.activation_func(final_inputs)
+
+
